812d9baa048ad8a0db36dc76d1e9b5f52f4f6776
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2024-10-27 15:56:58 +0100

    user crud finish

--- server/crud.py
+++ server/crud.py
@@ -26,3 +26,30 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def path_user(db: Session, user: schemas.UserCreate):
+    db_user = db.query(models.User).filter(models.User.user_email == user.user_email).first()
+
+    if db_user is None:
+        return False
+
+    try:
+        if user.user_name:
+            db_user.user_name = user.user_name
+        if user.user_email:
+            db_user.user_email = user.user_email.lower()
+        if user.password:
+            hashed_password = pwd_context.hash(user.password)
+            db_user.password = hashed_password
+
+        db.commit()
+        db.refresh(db_user)
+    except Exception as e:
+        db.rollback()
+        print(f"Error: {e}")
+        return False
+
+    return db_user
+
+
+# Gesture CRUD
