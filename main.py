83a85179af78e5fdc3d4d05a285030298b39bc6c
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2024-11-24 01:02:10 +0100

    chore: Clear files

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from server.routers.mongo import user as mongo_user_router
 from server.routers.mongo import device as mongo_device_router
 from server.routers.mongo import device_gesture as mongo_device_gesture_router
 from server.routers.mongo import log as mongo_gesture_log_router
-from server.routers import import_data as import_data_router, basic, user, gesture, device, usergesture
+from server.routers.postgresql import basicRouter, userRouter, gestureRouter, deviceRouter, userGestureRouter
+from server.core import database
 from server.core.mongo.database import connect_to_mongo, close_mongo_connection
 
 load_dotenv()
@@ -22,15 +23,15 @@ app = FastAPI()
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
 
-app.include_router(basic.router)
+app.include_router(basicRouter.router)
 
-app.include_router(user.router)
+app.include_router(userRouter.router)
 
-app.include_router(gesture.router)
+app.include_router(gestureRouter.router)
 
-app.include_router(device.router)
+app.include_router(deviceRouter.router)
 
-app.include_router(usergesture.router)
+app.include_router(userGestureRouter.router)
 
 app.include_router(mongo_user_router.router, prefix="/mongo/users", tags=["users"])
 app.include_router(mongo_device_router.router, prefix="/mongo/devices", tags=["devices"])
