f27aaf4bc724060ac77e642fde5a5fbde46a3924
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2024-12-03 22:26:14 +0100

    add: Swagger example value schema

--- server/schemas/db_data.py
+++ server/schemas/db_data.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ImportRequest(BaseModel):
-    user_count: int
-    device_count_range: tuple[int, int]
-    gesture_count_range: tuple[int, int]
-    log_count: int
+    user_count: int = Field(..., examples=[100])
+    device_count_range: tuple[int, int] = Field(..., examples=[(0, 5)])
+    gesture_count_range: tuple[int, int] = Field(..., examples=[(0, 5)])
+    log_count: int = Field(..., examples=[5])
--- server/schemas/mongo/device.py
+++ server/schemas/mongo/device.py
@@ -22,10 +22,10 @@ class DeviceBase(BaseModel):
 
 
 class DeviceCreate(BaseModel):
-    device_type: str
-    device_name: str
+    device_type: str = Field(..., examples=["Light"])
+    device_name: str = Field(..., examples=["Light 1"])
     device_gestures: List[DeviceGestureBase] = Field(default_factory=list)
-    owner_id: PyObjectId
+    owner_id: PyObjectId = Field(..., examples=["60a5e2e3b8d5f6b8e3f8e5f6"])
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
@@ -34,9 +34,8 @@ class DeviceCreate(BaseModel):
 
 
 class DeviceUpdate(BaseModel):
-    device_type: Optional[str] = None
-    device_name: Optional[str] = None
-    # device_gestures: Optional[List[DeviceGestureBase]] = Field(default_factory=list)
+    device_type: Optional[str] = Field(None, examples=["Light"])
+    device_name: Optional[str] = Field(None, examples=["Light 1"])
 
 
 class DeviceOut(DeviceBase):
@@ -44,13 +43,13 @@ class DeviceOut(DeviceBase):
 
 
 class DeviceIDsRequest(BaseModel):
-    device_ids: List[str]
+    device_ids: List[str] = Field(..., examples=[["60a5e2e3b8d5f6b8e3f8e5f6", "60a5e2e3b8d5f6b8e3f8e5f7"]])
 
 
 class UserIDsRequest(BaseModel):
-    user_ids: List[str]
+    user_ids: List[str] = Field(..., examples=[["60a5e2e3b8d5f6b8e3f8e5f6", "60a5e2e3b8d5f6b8e3f8e5f7"]])
 
 
 class BulkDeviceUpdate(BaseModel):
-    device_ids: List[str]
+    device_ids: List[str] = Field(..., examples=[["60a5e2e3b8d5f6b8e3f8e5f6", "60a5e2e3b8d5f6b8e3f8e5f7"]])
     update_data: DeviceUpdate
