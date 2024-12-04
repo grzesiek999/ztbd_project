afef88e4655eabb0ba98b68e50023730ae5fa3cc
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2024-12-04 19:42:05 +0100

    postgre cleaning added

--- server/core/postgresql/database.py
+++ server/core/postgresql/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql+asyncpg://grzesiekpasich:@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/mydatabase"
 
 
 engine = create_engine(DATABASE_URL)
