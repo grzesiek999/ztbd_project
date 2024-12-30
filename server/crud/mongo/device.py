099c7405c3cf72a9f145364becbc31cc756450da
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2024-12-30 20:01:53 +0100

    fix: env variables in fastapi container

--- server/crud/mongo/device.py
+++ server/crud/mongo/device.py
@@ -9,8 +9,6 @@ from schemas.mongo.device import DeviceCreate, DeviceUpdate
 from typing import List
 import time
 
-from server.data_generator import device_types
-
 
 def find_devices(db: Database, user_ids: List[str]) -> float:
     # Add 'list' method - to force the query to execute
