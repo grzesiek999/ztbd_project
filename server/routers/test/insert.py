3be0efa00d17fcfa556fe4ec8ef9a063c44b0630
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2025-01-01 10:13:31 +0100

    add: API INSERT gesture - mongo

--- server/routers/test/insert.py
+++ server/routers/test/insert.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import Session
 from core.mongo.database import get_db as get_mongo_db
 from core.postgresql import database as get_postgresql_db
 
-from schemas.test import SamplesAndRowsCount, ExecutionTime
-from schemas.postgresql import userSchemas, deviceSchemas, utils
+from schemas.test import SamplesCount, SamplesAndRowsCount, ExecutionTime
+from schemas.postgresql import userSchemas, deviceSchemas, gestureSchemas, utils
 from schemas.mongo.user import UserCreate as MongoUserCreate
 from schemas.mongo.device import DeviceCreate as MongoDeviceCreate
+from schemas.mongo.device_gesture import DeviceGestureCreate, BulkDeviceGesturesCreate
 
 from routers.test import utils as test_utils
 from data_generator import generate_user, generate_device, generate_device_gesture, generate_device_type
@@ -53,7 +54,7 @@ def insert_users(request: SamplesAndRowsCount, mongo_db: Database = Depends(get_
 
 @router.post("/device", response_model=ExecutionTime)
 def insert_devices(request: SamplesAndRowsCount, mongo_db: Database = Depends(get_mongo_db),
-                 postgresql_db: Session = Depends(get_postgresql_db.get_db)):
+                   postgresql_db: Session = Depends(get_postgresql_db.get_db)):
     samples_count = request.samples_count
     rows_count = request.rows_count
     user_id_map_df = test_utils.get_users_id_map()
@@ -76,7 +77,6 @@ def insert_devices(request: SamplesAndRowsCount, mongo_db: Database = Depends(ge
         device['device_type_id'] = device_type_id
         postgres_devices.append(deviceSchemas.DeviceCreate(**device))
 
-
     postgres_times = []
     mongo_times = []
 
@@ -90,3 +90,28 @@ def insert_devices(request: SamplesAndRowsCount, mongo_db: Database = Depends(ge
 
     return ExecutionTime(postgres_execution_times=postgres_times, mongo_execution_times=mongo_times)
 
+
+@router.post("/gesture", response_model=ExecutionTime)
+def insert_gestures(request: SamplesCount, mongo_db: Database = Depends(get_mongo_db),
+                   postgresql_db: Session = Depends(get_postgresql_db.get_db)):
+    samples_count = request.samples_count
+
+    device_type = generate_device_type()
+    gesture = generate_device_gesture()
+
+    mongo_gesture_and_devicetype = BulkDeviceGesturesCreate(device_type=device_type,
+                                                            gesture=DeviceGestureCreate(**gesture))
+    # postgres_device_gesture = gestureSchemas.DeviceGestureCreate(**gesture)
+
+    postgres_times = []
+    mongo_times = []
+
+    for i in range(samples_count):
+        mongo_time = mongo_insert_device_gestures(mongo_db, mongo_gesture_and_devicetype)
+        mongo_times.append(mongo_time)
+        # postgres_time = createDeviceGesturesTest(postgres_devices, postgresql_db)
+        # postgres_times.append(postgres_time)
+
+        drop_and_import_data(postgresql_db)
+
+    return ExecutionTime(postgres_execution_times=postgres_times, mongo_execution_times=mongo_times)
